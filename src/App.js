fd69b6202c858acc2eb627e8debe594fbbf7ebef
fernando <ferrnsndowillian@gmail.com>
2024-10-11 09:26:23 -0300

    introducao ao redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import redux from "react-redux"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const trocarTema = (tema) => {
+  return {
+    type: "TROCAR_TEMA",
+    payload: tema
+  };
+};
+
+const tarefasReducer = (state = [], action) => {
+  switch(action.type){
+    case "TROCAR_TEMA":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(tarefasReducer)
 
 export default App;
